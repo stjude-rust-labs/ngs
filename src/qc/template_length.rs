20a634b641a004a22050b241687aebf56f217c0f
Clay McLeod <Clay.McLeod@STJUDE.org>
2022-09-14 09:25:06 -0500

    qc/tlen: reorganize output struct

--- src/qc/template_length.rs
+++ src/qc/template_length.rs
@@ -9,6 +9,23 @@ use crate::utils::histogram::{BinOutOfBoundsError, SimpleHistogram};
 
 use super::{ComputationalLoad, Error, QualityCheckFacet};
 
+#[derive(Debug, Serialize)]
+pub struct SummaryMetrics {
+    template_length_unknown_pct: f64,
+    template_length_out_of_range_pct: f64,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RecordMetrics {
+    // Number of records that were processed (and, as such, had template lengths
+    // that fell within our histogram's range).
+    processed: usize,
+
+    // Number of records that were ignored (and, as such, had template lengths
+    // that fell outside of our histogram's range).
+    ignored: usize,
+}
+
 /// Primary struct used to compile stats regarding template length. Within this
 /// struct, the histogram represents the distribution of records with a particular
 /// template length up to a certain threshold. Any records that fall outside of
@@ -19,17 +36,8 @@ pub struct TemplateLengthFacet {
     // Histogram that represents the number of records that have a given
     // template length (up to the specified threshold).
     histogram: SimpleHistogram,
-
-    // Number of records that were processed (and, as such, had template lengths
-    // that fell within our histogram's range).
-    processed: usize,
-
-    // Number of records that were ignored (and, as such, had template lengths
-    // that fell outside of our histogram's range).
-    ignored: usize,
-
-    template_length_unknown_pct: Option<f64>,
-    template_length_out_of_range_pct: Option<f64>,
+    record_metrics: RecordMetrics,
+    summary: Option<SummaryMetrics>,
 }
 
 impl TemplateLengthFacet {
@@ -37,10 +45,11 @@ impl TemplateLengthFacet {
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
             histogram: SimpleHistogram::zero_based_with_capacity(capacity),
-            processed: 0,
-            ignored: 0,
-            template_length_unknown_pct: None,
-            template_length_out_of_range_pct: None,
+            record_metrics: RecordMetrics {
+                processed: 0,
+                ignored: 0,
+            },
+            summary: None,
         }
     }
 
@@ -53,13 +62,13 @@ impl TemplateLengthFacet {
     #[allow(dead_code)]
     /// Gets the number of processed records.
     pub fn get_processed_count(&self) -> usize {
-        self.processed
+        self.record_metrics.processed
     }
 
     /// Gets the number of ignored records.
     #[allow(dead_code)]
     pub fn get_ignored_count(&self) -> usize {
-        self.ignored
+        self.record_metrics.ignored
     }
 }
 
@@ -79,19 +88,22 @@ impl QualityCheckFacet for TemplateLengthFacet {
     fn process(&mut self, record: &Record) -> Result<(), Error> {
         let template_len = record.template_length() as usize;
         match self.histogram.increment(template_len) {
-            Ok(()) => self.processed += 1,
-            Err(BinOutOfBoundsError) => self.ignored += 1,
+            Ok(()) => self.record_metrics.processed += 1,
+            Err(BinOutOfBoundsError) => self.record_metrics.ignored += 1,
         }
 
         Ok(())
     }
 
     fn summarize(&mut self) -> Result<(), Error> {
-        self.template_length_unknown_pct = Some(
-            (self.histogram.get(0) as f64 / (self.processed as f64 + self.ignored as f64)) * 100.0,
-        );
-        self.template_length_out_of_range_pct =
-            Some((self.ignored as f64 / (self.processed as f64 + self.ignored as f64)) * 100.0);
+        self.summary = Some(SummaryMetrics {
+            template_length_unknown_pct: (self.histogram.get(0) as f64
+                / (self.record_metrics.processed as f64 + self.record_metrics.ignored as f64))
+                * 100.0,
+            template_length_out_of_range_pct: (self.record_metrics.ignored as f64
+                / (self.record_metrics.processed as f64 + self.record_metrics.ignored as f64))
+                * 100.0,
+        });
 
         Ok(())
     }
