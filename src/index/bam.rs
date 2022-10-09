1556a23bf3ca7653ce457594a67c6e0d0244640d
Clay McLeod <clay.l.mcleod@gmail.com>
2022-10-08 21:04:03 -0500

    revise: adds debug statements and record counting to bam indexing

--- src/index/bam.rs
+++ src/index/bam.rs
@@ -8,7 +8,9 @@ use anyhow::{bail, Context};
 use noodles::bam::{self as bam, bai};
 use noodles::csi::index::reference_sequence::bin::Chunk;
 use noodles::sam::{self as sam, alignment::Record, header::record::value::map::header::SortOrder};
+use num_format::{Locale, ToFormattedString};
 use std::{fs::File, io, path::PathBuf};
+use tracing::{debug, info};
 
 use crate::utils::formats::sam::parse_header;
 use crate::utils::path::AppendExtension;
@@ -30,7 +32,10 @@ fn is_coordinate_sorted(header: &sam::Header) -> bool {
 
 /// Main method for BAM indexing.
 pub fn index(src: PathBuf) -> anyhow::Result<()> {
+    info!("indexing BAM file");
+
     // (1) Reads the file from disk.
+    debug!("reading BAM file from disk");
     let mut reader = File::open(&src)
         .map(bam::Reader::new)
         .with_context(|| "opening src file")?;
@@ -56,6 +61,7 @@ pub fn index(src: PathBuf) -> anyhow::Result<()> {
         .with_context(|| "reading reference sequences")?;
 
     // (4) Check if the BAM is coordinate sorted. If it isn't, then return an error.
+    debug!("checking the BAM is coordinate sorted");
     if !is_coordinate_sorted(&header) {
         return Err(io::Error::new(
             io::ErrorKind::InvalidData,
@@ -65,10 +71,11 @@ pub fn index(src: PathBuf) -> anyhow::Result<()> {
     }
 
     // (5) Build the BAM index.
+    debug!("building the BAM index");
     let mut record = Record::default();
-
     let mut builder = bai::Index::builder();
     let mut start_position = reader.virtual_position();
+    let mut records_processed = 0;
 
     loop {
         match reader.read_record(&mut record) {
@@ -85,11 +92,20 @@ pub fn index(src: PathBuf) -> anyhow::Result<()> {
             .with_context(|| "building BAM index")?;
 
         start_position = end_position;
+        records_processed += 1;
+
+        if records_processed % 1_000_000 == 0 {
+            debug!(
+                "  [*] Processed {} records.",
+                records_processed.to_formatted_string(&Locale::en),
+            );
+        }
     }
 
     let index = builder.build(header.reference_sequences().len());
 
     // (6) Write the index to disk.
+    debug!("writing the BAM index to disk");
     let mut writer = File::create(bai)
         .map(bai::Writer::new)
         .with_context(|| "creating BAM index output file")?;
--- src/index/fasta.rs
+++ src/index/fasta.rs
@@ -9,10 +9,18 @@ use std::{fs::File, path::PathBuf};
 
 use anyhow::{bail, Context};
 use noodles::fasta::{self, fai};
+use tracing::{debug, info};
 
 /// Indexes a FASTA file stored at `src`.
 pub fn index(src: PathBuf) -> anyhow::Result<()> {
+    info!("indexing FASTA file");
+
+    // (1) Reads the file from disk.
+    debug!("reading FASTA file from disk");
     let index = fasta::index(&src).with_context(|| "building FASTA index")?;
+
+    // (2) Calculate where the FASTA index should go and check if a file is
+    // already there. Error out if so.
     let fai = src
         .append_extension("fai")
         .with_context(|| "building filepath for FASTA index")?;
@@ -25,6 +33,8 @@ pub fn index(src: PathBuf) -> anyhow::Result<()> {
         );
     }
 
+    // (3) Write the index to disk.
+    debug!("writing the FASTA index to disk");
     let mut writer = File::create(fai)
         .map(fai::Writer::new)
         .with_context(|| "initializing the new FASTA index file")?;
